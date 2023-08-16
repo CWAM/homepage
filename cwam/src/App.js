cfc3100308a5e7ee112f1198e1491f44b61b631d
slacker525600 <slacker525600@yahoo.com>
2023-08-16 14:00:21 -0500

    Removed react link and added cwam backronyms

--- cwam/src/App.js
+++ cwam/src/App.js
@@ -9,14 +9,30 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          CWAM backronyms:
+Canned Wine Appreciation Manager
+Cash Wules(everything) Around Me
+Coder Warned About Management
+Crazy (how) We All Manage
+Couldn’t We All Mutiny
+Capitalism will annihilate mankind
+Cute Weasels Attack Monday
+Comedy Writers Anonymous Meeting
+Can't work all memes
+Cacophonous wailing and misfortune
+Catastrophic workplace accident minions (edited) 
+Coulda woulda almost mighta
+Cloud work antagonism meetup
+Come Wander Among Misanthropes
+Can’t Wager Against Monkeys
+Catharsis When Alex Moves
+Calm Workers Are Magical
+Can't we all Montys
+Caligula Wasn't A Mensch
+Cleopatra Wrote a Manga
+Commonly wasteful and meandering
+Cyber Wankery and Malice
+Constantly wondering about Michael
       </header>
     </div>
   );
